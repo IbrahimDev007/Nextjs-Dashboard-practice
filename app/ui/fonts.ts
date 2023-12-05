544eb4fecb9de01bf71f399ddad97487ecb40f90
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-12-05 12:36:43 +0600

    font add

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({
+  weight: ['400', '700', '300', '100', '200'],
+  subsets: ['latin'],
+});
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
